7d43b576c4601452ce9f83905bc93c2a64ac8e0e
unknown <wumi.olabisi@gmail.com>
2024-07-11 11:14:19 +0200

    Ajouts de projets dans le portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Layout from '../components/layouts/main';
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+import Fonts from '../components/fonts'
 import theme from '../lib/theme'
 
 
 const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
+            <Fonts />
             <Layout router={router}>
                 <Component {...pageProps} key={router.route} />
             </Layout>
---
+++ pages/works.js
@@ -0,0 +1,29 @@
+import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumbpage28 from '../public/images/works/page28-wp.png'
+import thumbkroskel from '../public/images/works/thumbkroskel.png'
+const Works = () => {
+    return (
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Projets
+            </Heading>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="page28" title="Page 28" thumbnail={thumbpage28}>
+                        Un catalogue de films et séries exclusivement réalisées par des femmes
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="kroskel" title="La boutique Kroskel" thumbnail={thumbkroskel}>
+                        Une marque de vêtements franco-camerounaise pour les femmes audacieuses
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+        </Container>
+    )
+
+}
+
+export default Works
